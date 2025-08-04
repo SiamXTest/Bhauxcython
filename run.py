0822f1ad38a142f55e0daad50038b9f2eb626581
SiamXTest <sifamoni169@gmail.com>
2025-08-04 18:23:24 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,13 +1,13 @@
 import BhauTheUltimateTools
 
-# ফাংশন তালিকা দেখা
+
 print("Available functions/objects in BhauTheUltimateTools:")
 print(dir(BhauTheUltimateTools))
 
-# যদি main() থাকে
+
 if hasattr(BhauTheUltimateTools, "main"):
     BhauTheUltimateTools.main()
 
-# অথবা যদি start() থাকে
+
 elif hasattr(BhauTheUltimateTools, "start"):
     BhauTheUltimateTools.start()
